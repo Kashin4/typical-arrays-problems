b20caf59ad608b3afd45f12fbe3898921c1cbabc
Ihar Semashka <6399815@mail.ru>
2021-03-16 00:17:40 +0300

    init: start task arrays problem

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-
+//--------------------min
 exports.min = function min (array) {
-  return 0;
+  if(!array || array.length === 0){
+    return 0;
+  }
+
+  let min = array[0];
+  for(let i = 1; i < array.length;i++){
+    if(min > array[i]){
+      min = array[i];
+    }
+  }
+  return min;
 }
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
