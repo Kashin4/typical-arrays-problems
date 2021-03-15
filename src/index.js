18fe2aa22b8ac09f431ea2c1610bb9445b17ddd3
Ihar Semashka <6399815@mail.ru>
2021-03-16 00:41:44 +0300

    feat: add max avg

--- src/index.js
+++ src/index.js
@@ -13,3 +13,37 @@ exports.min = function min (array) {
   return min;
 }
 
+//--------------------max
+
+exports.max = function max (array) {
+  if(!array || array.length === 0){
+    return 0;
+  }
+
+  let max = array[0];
+  for(let i = 1; i < array.length;i++)
+  {
+    if(max < array[i])
+    {
+      max = array[i];
+    }
+  }
+
+  return max;
+}
+//--------------------avg
+exports.avg = function avg (array) {
+  if(!array){
+    return 0;
+  }
+
+  let avg = 0;
+  let count = 0;
+  for (let i = 0; i < array.length;i++)
+  {
+      avg += array[i];
+      count++;
+  }
+
+  return count > 0 ? avg / count : 0;
+}
